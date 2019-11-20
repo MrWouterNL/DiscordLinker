63253c2bdddf9e1d66cec81c77448bbf25529e16
ljavanbeek <info@lucasvanbeek.nl>
2019-11-20 21:27:59 +0100

    Update Main.java

--- src/main/java/nl/minetopiasdb/discordlinker/Main.java
+++ src/main/java/nl/minetopiasdb/discordlinker/Main.java
@@ -135,7 +135,8 @@ public class Main extends JavaPlugin {
         try {
             bot = builder.build();
         } catch (Exception ex) {
-            if (ex.getMessage().contains("401: Unauthorized")) {
+            getLogger().info(ex.getMessage());
+            if (ex.getMessage().contains("The provided token is invalid")) {
                 Bukkit.getPluginManager().registerEvents(new Listener() {
                     @EventHandler
                     public void onJoin(PlayerJoinEvent e) {
