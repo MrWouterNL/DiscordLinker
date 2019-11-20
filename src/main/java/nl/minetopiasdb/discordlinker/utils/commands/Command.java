fb8160c106c4ade2ee54e715ec2283fde1b90c8b
ljavanbeek <info@lucasvanbeek.nl>
2019-11-20 19:56:35 +0100

    Commandhandler yaay

---
+++ src/main/java/nl/minetopiasdb/discordlinker/utils/commands/Command.java
@@ -0,0 +1,14 @@
+package nl.minetopiasdb.discordlinker.utils.commands;
+
+public class Command {
+
+    String name;
+
+    public Command(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
