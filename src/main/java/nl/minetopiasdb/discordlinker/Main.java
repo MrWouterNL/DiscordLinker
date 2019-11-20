f4dd71a5531f3d4eb2a2e545b2ea8323171fd728
ljavanbeek <info@lucasvanbeek.nl>
2019-11-20 18:01:53 +0100

    Main class converted to JDA

--- src/main/java/nl/minetopiasdb/discordlinker/Main.java
+++ src/main/java/nl/minetopiasdb/discordlinker/Main.java
@@ -1,19 +1,11 @@
 package nl.minetopiasdb.discordlinker;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-
-import org.apache.commons.io.FileUtils;
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.managers.Presence;
 import nl.minetopiasdb.discordlinker.bukkit.commands.LinkCMD;
 import nl.minetopiasdb.discordlinker.bukkit.listeners.JoinListener;
 import nl.minetopiasdb.discordlinker.discord.commands.DiscordLinkCMD;
@@ -23,124 +15,155 @@ import nl.minetopiasdb.discordlinker.discord.commands.StatCMD;
 import nl.minetopiasdb.discordlinker.utils.UpdateChecker;
 import nl.minetopiasdb.discordlinker.utils.data.ConfigUtils;
 import nl.minetopiasdb.discordlinker.utils.data.UserData;
-import sx.blah.discord.api.ClientBuilder;
-import sx.blah.discord.api.IDiscordClient;
-import sx.blah.discord.handle.obj.ActivityType;
-import sx.blah.discord.handle.obj.StatusType;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 public class Main extends JavaPlugin {
 
-	private static Plugin plugin;
-	private static IDiscordClient bot;
+    private static Plugin plugin;
+    private static JDA bot;
 
-	public void onEnable() {
-		plugin = this;
-		if (!Bukkit.getPluginManager().isPluginEnabled("MinetopiaSDB")) {
-			Bukkit.getPluginManager().registerEvents(new Listener() {
-				@EventHandler
-				public void onJoin(PlayerJoinEvent e) {
-					if (e.getPlayer().isOp()) {
-						Bukkit.getScheduler().runTaskLater(Main.plugin, new Runnable() {
-							public void run() {
-								e.getPlayer().sendMessage(ChatColor.RED
-										+ "DiscordLinker >> MinetopiaSDB is vereist om DiscordLinker te laten werken!");
-							}
-						}, 5 * 20l);
-					}
-				}
-			}, Main.plugin);
-		}
-		ConfigUtils.getInstance().addDefault(plugin);
-		UserData.getInstance().setup(plugin);
-		loginBot();
-		getCommand("link").setExecutor(new LinkCMD());
-		Bukkit.getPluginManager().registerEvents(new UpdateChecker(), this);
-		Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
-	}
+    public static JDA getBot() {
+        return bot;
+    }
 
-	public void loginBot() {
-		try {
-			File libs = new File(getDataFolder() + File.separator + "libs");
-			if (!libs.exists()) {
-				libs.mkdirs();
-			}
-			File file = new File(getDataFolder() + File.separator + "libs" + File.separator + "Discord4J.jar");
-			if (!file.exists()) {
-				getLogger().info("Discord4j not found in libs folder! Downloading..");
-				FileUtils.copyURLToFile(new URL("https://content.minetopiasdb.nl/discordlinker/Discord4J.jar"), file, 10000, 10000);
-				if (!file.exists() || file.getTotalSpace() == 0) {
-					getLogger().warning(
-							"Downloading Discord4J failed, please try again. Are you sure content.minetopiasdb.nl is accessible?");
-					return;
-				} else {
-					getLogger().info("Discord4J download complete!");
-				}
-			}
+    public static Plugin getPlugin() {
+        return plugin;
+    }
 
-			URL discord = file.toURI().toURL();
-			URLClassLoader ucl = (URLClassLoader) getClassLoader();
-			Method add = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-			add.setAccessible(true);
-			add.invoke(ucl, discord);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+    public static void updatePresence() {
+        Presence presence = getBot().getPresence();
 
-		ClientBuilder client = new ClientBuilder();
-		String token = ConfigUtils.getInstance().getBotToken();
-		try {
-			client.withToken(token);
-			bot = client.login();
-		} catch (Exception ex) {
-			if (ex.getMessage().contains("401: Unauthorized")) {
-				Bukkit.getPluginManager().registerEvents(new Listener() {
-					@EventHandler
-					public void onJoin(PlayerJoinEvent e) {
-						if (e.getPlayer().isOp()) {
-							Bukkit.getScheduler().runTaskLater(Main.plugin, new Runnable() {
-								public void run() {
-									e.getPlayer().sendMessage(ChatColor.RED
-											+ "DiscordLinker >> Geen geldige discordtoken ingesteld. De plugin doet nu niets!");
-									e.getPlayer().sendMessage(ChatColor.RED
-											+ "Voor info: https://projects.minetopiasdb.nl/discordlinker");
-								}
-							}, 5 * 20l);
-						}
-					}
-				}, Main.plugin);
-				Main.plugin.getLogger().warning("GEEN GELDIGE DISCORDTOKEN INGESTELD!");
-				return;
-			}
-			ex.printStackTrace();
-		}
-		bot.getDispatcher().registerListener(new DiscordLinkCMD());
-		bot.getDispatcher().registerListener(new StatCMD());
-		bot.getDispatcher().registerListener(new ServerinfoCMD());
-		bot.getDispatcher().registerListener(new HelpCMD());
+        presence.setStatus(OnlineStatus.valueOf(ConfigUtils.getInstance().getStatus()));
+        switch(ConfigUtils.getInstance().getPlayingType().toLowerCase()) {
+            case "playing":
+                presence.setActivity(Activity.playing(ConfigUtils.getInstance().getPlayingMessage().replaceAll("<Spelers>", "" + Bukkit.getOnlinePlayers().size())));
+                break;
+            case "watching":
+                presence.setActivity(Activity.watching(ConfigUtils.getInstance().getPlayingMessage().replaceAll("<Spelers>", "" + Bukkit.getOnlinePlayers().size())));
+                break;
+            case "listening":
+                presence.setActivity(Activity.listening(ConfigUtils.getInstance().getPlayingMessage().replaceAll("<Spelers>", "" + Bukkit.getOnlinePlayers().size())));
+                break;
+            case "streaming":
+                presence.setActivity(Activity.streaming(ConfigUtils.getInstance().getPlayingMessage().replaceAll("<Spelers>", "" + Bukkit.getOnlinePlayers().size()), null));
+                break;
+        }
+    }
 
-		while (!bot.isReady()) {
-			//wait a sec.
-		}
-		updatePresence();
-	}
+    public void onEnable() {
+        plugin = this;
+        if (!Bukkit.getPluginManager().isPluginEnabled("MinetopiaSDB")) {
+            Bukkit.getPluginManager().registerEvents(new Listener() {
+                @EventHandler
+                public void onJoin(PlayerJoinEvent e) {
+                    if (e.getPlayer().isOp()) {
+                        Bukkit.getScheduler().runTaskLater(Main.plugin, new Runnable() {
+                            public void run() {
+                                e.getPlayer().sendMessage(ChatColor.RED
+                                        + "DiscordLinker >> MinetopiaSDB is vereist om DiscordLinker te laten werken!");
+                            }
+                        }, 5 * 20l);
+                    }
+                }
+            }, Main.plugin);
+        }
+        ConfigUtils.getInstance().addDefault(plugin);
+        UserData.getInstance().setup(plugin);
+        loginBot();
+        getCommand("link").setExecutor(new LinkCMD());
+        Bukkit.getPluginManager().registerEvents(new UpdateChecker(), this);
+        Bukkit.getPluginManager().registerEvents(new JoinListener(), this);
+    }
 
-	public void onDisable() {
-		if (bot != null && bot.isLoggedIn()) {
-			bot.logout();
-		}
-	}
+    public void loginBot() {
+        try {
+            File libs = new File(getDataFolder() + File.separator + "libs");
+            if (!libs.exists()) {
+                libs.mkdirs();
+            }
+            File jdafile = new File(getDataFolder() + File.separator + "libs" + File.separator + "JDA-4.0.0_62.jar");
+            if (!jdafile.exists()) {
+                File[] files = libs.listFiles();
+                for (File file : files) {
+                    if (file.getName().startsWith("JDA")) {
+                        file.delete();
+                    }
+                }
+                getLogger().info("Discord4j not found in libs folder! Downloading..");
+                FileUtils.copyURLToFile(new URL("https://ci.dv8tion.net/job/JDA/62/artifact/build/libs/JDA-4.0.0_62-withDependencies-no-opus.jar"), jdafile, 10000, 10000);
+                if (!jdafile.exists() || jdafile.getTotalSpace() == 0) {
+                    getLogger().warning(
+                            "Downloading JDA failed, please try again. Are you sure ci.dv8tion.net is accessible?");
+                    return;
+                } else {
+                    getLogger().info("JDA download complete!");
+                }
+            }
 
-	public static IDiscordClient getBot() {
-		return bot;
-	}
+            URL discord = jdafile.toURI().toURL();
+            URLClassLoader ucl = (URLClassLoader) getClassLoader();
+            Method add = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            add.setAccessible(true);
+            add.invoke(ucl, discord);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-	public static Plugin getPlugin() {
-		return plugin;
-	}
-	
-	public static void updatePresence() {
-		getBot().changePresence(StatusType.valueOf(ConfigUtils.getInstance().getStatus()),
-				ActivityType.valueOf(ConfigUtils.getInstance().getPlayingType()),
-				ConfigUtils.getInstance().getPlayingMessage().replaceAll("<Spelers>", "" + Bukkit.getOnlinePlayers().size()));
-	}
+        String token = ConfigUtils.getInstance().getBotToken();
+        JDABuilder builder = new JDABuilder(AccountType.BOT)
+                .setToken(token)
+                .setActivity(Activity.playing("loading"))
+                .setStatus(OnlineStatus.DO_NOT_DISTURB);
+
+        bot.addEventListener(new DiscordLinkCMD());
+        bot.addEventListener(new StatCMD());
+        bot.addEventListener(new ServerinfoCMD());
+        bot.addEventListener(new HelpCMD());
+
+        try {
+            bot = builder.build();
+        } catch (Exception ex) {
+            if (ex.getMessage().contains("401: Unauthorized")) {
+                Bukkit.getPluginManager().registerEvents(new Listener() {
+                    @EventHandler
+                    public void onJoin(PlayerJoinEvent e) {
+                        if (e.getPlayer().isOp()) {
+                            Bukkit.getScheduler().runTaskLater(Main.plugin, new Runnable() {
+                                public void run() {
+                                    e.getPlayer().sendMessage(ChatColor.RED
+                                            + "DiscordLinker >> Geen geldige discordtoken ingesteld. De plugin doet nu niets!");
+                                    e.getPlayer().sendMessage(ChatColor.RED
+                                            + "Voor info: https://projects.minetopiasdb.nl/discordlinker");
+                                }
+                            }, 5 * 20l);
+                        }
+                    }
+                }, Main.plugin);
+                Main.plugin.getLogger().warning("GEEN GELDIGE DISCORDTOKEN INGESTELD!");
+                return;
+            }
+            ex.printStackTrace();
+        }
+
+        updatePresence();
+    }
+
+    public void onDisable() {
+
+        if (bot != null && bot.getStatus().isInit()) {
+            bot.shutdown();
+        }
+    }
 }
