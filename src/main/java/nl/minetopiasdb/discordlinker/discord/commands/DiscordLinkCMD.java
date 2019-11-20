350672f88319607005d36f7d264c71324979ab59
ljavanbeek <info@lucasvanbeek.nl>
2019-11-20 22:17:07 +0100

    Last fix rofl

--- src/main/java/nl/minetopiasdb/discordlinker/discord/commands/DiscordLinkCMD.java
+++ src/main/java/nl/minetopiasdb/discordlinker/discord/commands/DiscordLinkCMD.java
@@ -2,7 +2,6 @@ package nl.minetopiasdb.discordlinker.discord.commands;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.PrivateChannel;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import nl.minetopiasdb.discordlinker.utils.MessageUtils;
 import nl.minetopiasdb.discordlinker.utils.commands.BotCommand;
@@ -28,17 +27,20 @@ public class DiscordLinkCMD implements BotCommand {
         if (LinkUtils.getInstance().isValidLink(id)) {
             LinkUtils.getInstance().removeLink(id);
         }
+
         UUID linkUUID = LinkUtils.getInstance().registerLink(id);
-        Consumer<PrivateChannel> succes = (privateChannel) -> {
-            EmbedBuilder embed = MessageUtils.getBuilder(Color.GREEN)
-                    .setDescription("Type het volgende commando in Minecraft om jouw account te linken: ```/link " + linkUUID.toString() + "```");
-            privateChannel.sendMessage(embed.build()).queue();
+        Consumer<Message> success = (privateChannel) -> {
+
         };
         Consumer<Throwable> failure = (error) -> {
-            EmbedBuilder embed = MessageUtils.getBuilder(Color.RED).setDescription(event.getAuthor().getAsTag() + ", je moet je privéberichten aanzetten!");
+            EmbedBuilder embed = MessageUtils.getBuilder(Color.RED).setDescription(event.getAuthor().getAsMention() + ", je moet je privéberichten aanzetten!");
             event.getChannel().sendMessage(embed.build()).queue();
             LinkUtils.getInstance().removeLink(linkUUID);
         };
-        event.getAuthor().openPrivateChannel().queue(succes, failure);
+        event.getAuthor().openPrivateChannel().queue(privateChannel -> {
+            EmbedBuilder embed = MessageUtils.getBuilder(Color.GREEN)
+                    .setDescription("Type het volgende commando in Minecraft om jouw account te linken: ```/link " + linkUUID.toString() + "```");
+            privateChannel.sendMessage(embed.build()).queue(success, failure);
+        });
     }
 }
