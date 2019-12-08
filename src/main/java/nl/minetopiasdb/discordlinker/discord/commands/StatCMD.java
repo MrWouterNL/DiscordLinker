0a47e809b53585ab2ce2400973dbd76c8699b127
ljavanbeek <info@lucasvanbeek.nl>
2019-12-08 20:16:40 +0100

    Nah

--- src/main/java/nl/minetopiasdb/discordlinker/discord/commands/StatCMD.java
+++ src/main/java/nl/minetopiasdb/discordlinker/discord/commands/StatCMD.java
@@ -93,8 +93,7 @@ public class StatCMD implements BotCommand {
             embed.addField(cu.getShowTitle(ShowOption.GRAYCOIN), "" + sdb.getGrayCoins(), false);
         }
         if (cu.getShowOption(ShowOption.LUCKYSHARD)) {
-            double shards = Math.round(sdb.getShards() * 10000.0) / 10000.0;
-            embed.addField(cu.getShowTitle(ShowOption.LUCKYSHARD), "" + shards, false);
+            embed.addField(cu.getShowTitle(ShowOption.LUCKYSHARD), "" + sdb.getShards(), false);
         }
 
         event.getChannel().sendMessage(embed.build()).queue();
